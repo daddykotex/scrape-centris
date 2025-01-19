f0ae6bca90688df0d731982f53f9a02a41c332c3
David Francoeur <dfrancoeur04@gmail.com>
2025-01-19 08:01:57 -0500

    feat: extract info from page

--- scrape_centris/spiders/centris_plex.py
+++ scrape_centris/spiders/centris_plex.py
@@ -1,4 +1,5 @@
 import json
+from typing import Optional
 
 import scrapy
 
@@ -54,7 +55,53 @@ class CentrisPlexSpider(scrapy.Spider):
     # https://www.centris.ca/fr/triplex~a-vendre~montreal-saint-laurent/15096301?view=Summary
     def parse_property_details(self, response):
         last_part = response.url.split("/")[-1]
+
+        address = clean_text(response.css("h2[itemprop=address]::text").get())
+        price = clean_text(response.css("#BuyPrice::text").get())
+
+        # caracteristics
+        def get_div_next_to_text(text: str):
+            sanitized_text = text.replace('"', '"')
+            return response.xpath(
+                f'//div[contains(text(), "{sanitized_text}")]/following-sibling::div'
+            )
+
+        characteristic_names = [
+            "Année de construction",
+            "Superficie du terrain",
+            "Stationnement total",
+            "Nombre d'unités",
+            "Unités résidentielles",
+            "Revenus bruts potentiels",
+        ]
+        characteristics = {
+            name: clean_text(get_div_next_to_text(name).css("span::text").get())
+            for name in characteristic_names
+        }
+
+        def get_financial_details():
+            table = response.css(".financial-details-table > table > tbody")
+            rows = table.css("tr")
+            return {
+                name: value
+                for row in rows
+                if (name := clean_text(row.css("td:nth-child(1)::text").get()))
+                and (value := clean_text(row.css("td:nth-child(2)::text").get()))
+            }
+
+        financial_details = get_financial_details()
+
         property_id = last_part.split("?")[0]
         yield {
             "property_id": property_id,
+            "address": address,
+            "price": price,
+            "characteristics": characteristics,
+            "financial_details": financial_details,
         }
+
+
+def clean_text(text: Optional[str]):
+    if text is None:
+        return None
+    return text.strip().replace("\xa0", "")
