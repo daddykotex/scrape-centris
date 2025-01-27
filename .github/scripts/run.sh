6a354ee776f9e071c633449652d228833e555431
David Francoeur <dfrancoeur04@gmail.com>
2025-01-26 19:04:35 -0500

    feat: upload result as a release on github

--- .github/scripts/run.sh
+++ .github/scripts/run.sh
@@ -2,13 +2,10 @@
 
 set -euo pipefail
 
-CURRENT_DATETIME=$(date +"%Y%m%d_%H%M%S")
-OUTPUT_FILE="listings_${CURRENT_DATETIME}.json"
-
-
-if [ -n "${CI:-}" ]; then
-    echo "Setting OUTPUT_FILE environment variable" >&2
-    echo "OUTPUT_FILE=${OUTPUT_FILE}" >> "$GITHUB_ENV"
+if [ -z "${OUTPUT_FILE:-}" ]; then
+    echo "ERROR: OUTPUT_FILE environment variable is not set" >&2
+    exit 1
 fi
+
 
 poetry run scrapy crawl centris-plex -O "${OUTPUT_FILE}"
